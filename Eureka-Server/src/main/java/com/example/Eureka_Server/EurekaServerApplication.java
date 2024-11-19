7f306472323edd9ab9315fa7b74db2e5f4654fd0
OmarSaez <omar.saez@usach.cl>
2024-11-19 11:49:37 -0300

    Se corrige Confing-Service y queda pendiente que Eureka tome el bootstrap.yaml

--- Eureka-Server/src/main/java/com/example/Eureka_Server/EurekaServerApplication.java
+++ Eureka-Server/src/main/java/com/example/Eureka_Server/EurekaServerApplication.java
@@ -2,9 +2,11 @@ package com.example.Eureka_Server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @EnableEurekaServer
 public class EurekaServerApplication {
 
