ac78a54ba6a701bf514eb0bf2a4eb1eac91aea1b
Omar Saez <omar.saez@usach.cl>
2024-11-20 11:43:19 -0300

    Config-service funcioanndo y se tuvo que cambiar el nombre de eureka-service a Eureka-Server para que arranque

--- Eureka-Server/src/main/java/com/example/Eureka_Server/EurekaServerApplication.java
+++ Eureka-Server/src/main/java/com/example/Eureka_Server/EurekaServerApplication.java
@@ -2,11 +2,9 @@ package com.example.Eureka_Server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
 
