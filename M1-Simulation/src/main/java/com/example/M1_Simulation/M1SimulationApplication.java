c21de75779905036451090e35ccb954d33b190da
Omar Saez <omar.saez@usach.cl>
2024-11-23 03:19:45 -0300

    Se actualiza M1-Simulation de config-data

--- M1-Simulation/src/main/java/com/example/M1_Simulation/M1SimulationApplication.java
+++ M1-Simulation/src/main/java/com/example/M1_Simulation/M1SimulationApplication.java
@@ -2,10 +2,8 @@ package com.example.M1_Simulation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class M1SimulationApplication {
 
 	public static void main(String[] args) {
